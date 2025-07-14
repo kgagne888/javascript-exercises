159e7dc93c30c15282cc25d322c2b2d95ebd8ca1
kgagne888 <kgagne888@gmail.com>
2025-07-14 14:38:48 -0600

    Complete lesson 08_removeFromArray

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,5 +1,6 @@
-const removeFromArray = function() {
-};
+const removeFromArray = function(arr, ...args) {
+    return arr.filter(val => !args.includes(val));
+}; 
 
 // Do not edit below this line
 module.exports = removeFromArray;
